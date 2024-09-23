239998585c2ea9766d005e14d3ec4a5b13942037
GwonDooHyeon <secure3141@gmail.com>
2024-09-23 19:46:22 +0900

    init project

---
+++ src/main/java/com/bookpago/BookpagoApplication.java
@@ -0,0 +1,13 @@
+package com.bookpago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookpagoApplication.class, args);
+    }
+
+}
